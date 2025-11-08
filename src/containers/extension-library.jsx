392d8f37e8a66e4d41420791d913f7555cd49cdb
LilyMakesThings <127533508+LilyMakesThings@users.noreply.github.com>
2025-11-08 01:53:11 +0000

    pen group extension gallery (#96)

--- src/containers/extension-library.jsx
+++ src/containers/extension-library.jsx
@@ -8,7 +8,8 @@ import log from '../lib/log';
 import extensionLibraryContent, {
     galleryError,
     galleryLoading,
-    galleryMore
+    galleryMore,
+    penGroupGallery
 } from '../lib/libraries/extensions/index.jsx';
 import extensionTags from '../lib/libraries/tw-extension-tags';
 import galleryInsetIcon from '../lib/libraries/extensions/gallery/tw-icon-small.svg';
@@ -176,6 +177,9 @@ class ExtensionLibrary extends React.PureComponent {
                 library.push(toLibraryItem(galleryError));
             } else {
                 library.push(toLibraryItem(galleryLoading));
+            }
+            if (this.state.gallery) {
+                library.push(toLibraryItem(penGroupGallery))
             }
         }
 
