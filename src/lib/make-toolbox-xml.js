0a0b01bf4b23a4d10ccdf782ae39c324c195e25a
LilyMakesThings <vgmhut@gmail.com>
2026-04-10 01:14:55 +0100

    update deps && toolbox

--- src/lib/make-toolbox-xml.js
+++ src/lib/make-toolbox-xml.js
@@ -418,7 +418,6 @@ const control = function (isInitialSetup, isStage, targetId, colors) {
         <block type="control_if_else"/>
         <block id="wait_until" type="control_wait_until"/>
         <block id="repeat_until" type="control_repeat_until"/>
-        <block id="while" type="control_while"/>
         ${blockSeparator}
         <block id="all_at_once" type="control_all_at_once"/>
         ${blockSeparator}
